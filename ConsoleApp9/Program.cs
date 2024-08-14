d6b8b13c29f6128d2a0ad98f5d131b86a4b92e7b
Lore <Lore@DESKTOP-J005ERJ>
2024-08-14 16:51:33 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp9;
+
+Veicoli veicoli = new Veicoli();
+
+veicoli.Targa = "FF120TW";
+veicoli.Marca = "Fiat";
+veicoli.Modello = "Punto";
+veicoli.numeroposti = 3; 
+
+Console.WriteLine("Targa : " + veicoli.Targa);
+Console.WriteLine("Marca :" + veicoli.Marca);
+Console.WriteLine("Modello : " + veicoli.Modello);
+Console.WriteLine("NumeroPosti : " + veicoli.numeroposti);
+
+veicoli.Targa = "DD503TD";
+veicoli.Marca = "Scania";
+veicoli.Modello = "Twist";
+veicoli.qcapacita = 300;
+Console.WriteLine("Targa : " + veicoli.Targa);
+Console.WriteLine("Marca : " + veicoli.Marca);
+Console.WriteLine("Modello : " + veicoli.Modello);
+Console.WriteLine("Capacita massima di carico in quintali : " + veicoli.qcapacita);
+
---
+++ ConsoleApp9/veicoli.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Veicoli
+    {
+        public string Targa;
+        public string Marca;
+        public string Modello;
+        public int numeroposti;
+        public int qcapacita;
+        public Veicoli()
+        {
+            Targa = string.Empty;
+            Marca = string.Empty;
+            Modello = string.Empty;
+        }
+
+        public class Autocarri : Veicoli 
+        {
+            
+            public Autocarri()
+            {
+                qcapacita = 0;
+            }
+        }
+        public class Autoveicoli : Veicoli
+        {
+            public Autoveicoli()
+            {
+                base.numeroposti = 0;
+            }
+        }
+
+
+
+    }
+}
